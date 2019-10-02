2146b4dd5fb85b1c00a6bdb899e1f12c979df2de
jpb1394 <jpb1394@rit.edu>
2019-10-02 14:19:42 -0400

    Game Basics
    Added constant movement to a cube in our scene, and added a camera that will follow that cube. The cube will only have force added to it if it is on the ground. Also added three buttons that change the cube's "form" between Rock, Slime, and Balloon

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject objectToFollow;  // This is the object the camera will be following
+
+    public float zDistanceFromObject;  // The z distance at which we will be following our object
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = objectToFollow.transform.position;
+        this.transform.position = new Vector3(newPos.x, newPos.y, zDistanceFromObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // The current form this character is in
+    public enum Form { Rock, Slime, Balloon};
+    public Form playerForm;
+
+    public float currentSpeed; // The player's current speed
+    public float maxSpeed;  // The maximum speed out player can move
+    public float constantSpeed; // The speed that we want the player to constantly be moving at to the right
+    public float distanceToGround; // The distance at which the player is considered "on the ground." Read from the center of the player
+    public bool grounded;  // Whether the player is on the ground or not
+
+    private Rigidbody rb;   // The player's rigidbody which we will apply to forces to
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerForm = Form.Rock;
+
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Check to see if we are grounded
+        grounded = IsPlayerOnGround();
+        if (grounded)
+        {
+            // Add a constant veloctiy to our player
+            rb.AddForce(new Vector3(constantSpeed, 0.0f, 0.0f));
+        }
+
+        // Update our current speed calculations
+        currentSpeed = rb.velocity.magnitude;
+    }
+
+    /// <summary>
+    /// This method checks to see if the player is considered grounded
+    /// </summary>
+    /// <returns></returns>
+    private bool IsPlayerOnGround()
+    {
+        Ray ray = new Ray(this.transform.position, new Vector3(0.0f, -1.0f, 0.0f));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)){
+            if (hit.distance <= distanceToGround)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Changes the player's active form between rock(0), slime(1), and balloon(2)
+    /// </summary>
+    /// <param name="newForm">The new form the player is to assume</param>
+    public void ChangeForm(int newForm)
+    {
+        this.playerForm = (Form)newForm;
+        Debug.Log("Changed Form To: " + playerForm);
+
+        switch (playerForm)
+        {
+            case Form.Rock:
+                break;
+            case Form.Slime:
+                break;
+            case Form.Balloon:
+                break;
+        }
+    }
+}
