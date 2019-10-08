6d5fb813252c3ff5dd3bdef453f38e01142c46d0
DESKTOP-45U25OH\yanke <mwh2719@rit.edu>
2019-10-08 18:51:02 -0400

    Flow Fields
    Started working on the code for flow fields in my test scene

---
+++ Assets/FlowField.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowField : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(this.gameObject.transform.position.x + this.GetComponent<Collider>().bounds.size.x/2 <= player.gameObject.transform.position.x + this.GetComponent<Collider>().bounds.size.x/2 
+            && this.gameObject.transform.position.x - this.GetComponent<Collider>().bounds.size.x/2  >= player.gameObject.transform.position.x - this.GetComponent<Collider>().bounds.size.x/2)
+        {
+            Debug.Log("Intersect");
+            player.gameObject.GetComponent<PlayerController>().constantSpeed += .2f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().constantSpeed += .2f;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().constantSpeed -= .2f;
+        }
+    }
+}
