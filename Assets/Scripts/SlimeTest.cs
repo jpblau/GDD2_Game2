3fcf98679a68d43b1f8738334bc744dbf3646294
Tomasay <42788482+Tomasay@users.noreply.github.com>
2019-10-22 22:23:05 -0400

    Added soft body assets, fixed outline materials

---
+++ Assets/Scripts/SlimeTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NVIDIA.Flex;
+
+public class SlimeTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Rigidbody>().velocity = new Vector3(4.0f, GetComponent<Rigidbody>().velocity.y);
+        Debug.Log("I AM MOVING");
+        //transform.Translate(1, 0, 0);
+
+        //GetComponent<FlexActor>().Teleport(transform.position + new Vector3(0.1f, 0, 0), Quaternion.identity);
+        GetComponent<FlexActor>().ApplyImpulse(new Vector3(100, 0, 0));
+    }
+}
